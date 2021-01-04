20bcb935e2f8fa2b36ca75768e1254a0f1411993
Nikolov0080 <apofis12343@gmail.com>
2021-01-05 01:50:45 +0300

    chreated: link to search results page

--- src/components/common/header/searchRecipe/search.js
+++ src/components/common/header/searchRecipe/search.js
@@ -2,51 +2,37 @@ import React, { useState } from 'react'
 import style from '../header.module.css';
 import search_icon from '../search_icon.png';
 import searchRecipe from '../../../../controllers/recipes/POST/searchRecipe/index';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Search = () => {
     const [query, setQuery] = useState('');
-    const [found, setFound] = useState([]);
 
-    async function search() {
-        return await searchRecipe(query);
-    }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        search().then((resp) => {
-
-            console.log(resp)
-            setFound(resp);
-        }).then(() => {
-
-        })
-    }
 
     const handleChange = (e) => {
         setQuery(e.target.value)
     }
 
-    const handleClick = (e) => {
-        if (query !== '') {
-
-        }
-    }
-    
     return (
         <div>
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <div className={style.search_container}>
-                    <input placeholder="Search recipe..."
-                        className={style.input_search}
-                        onChange={handleChange} type="text"
-                    />
-                    <button onClick={handleClick} className={style.search_btn}>
-                        <img className={style.search_icon_style} src={search_icon} alt="search" />
-                    </button>
-                </div>
+            <div className={style.search_container}>
+                <input placeholder="Search recipe..."
+                    className={style.input_search}
+                    onChange={handleChange} type="text"
+                />
 
-            </form>
+                {query !== ''
+                    ?
+                    <LinkContainer to={"/search-results/query=?" + query}>
+                        <button className={style.search_btn}>
+                            <img className={style.search_icon_style} src={search_icon} alt="search" />
+                        </button>
+                    </LinkContainer>
+                    :
+                        <img className={style.search_icon_style} src={search_icon} alt="search" />
+                }
+
+            </div>
         </div>
     )
 }
--- src/pages/recipes/searchResults/index.js
+++ src/pages/recipes/searchResults/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Component } from 'react'
 import ResultList from './resultList'
+import searchRecipe from '../../../controllers/recipes/POST/searchRecipe/index';
 
 class SearchResults extends Component {
 
+    
+    
     render() {
 
         return (
