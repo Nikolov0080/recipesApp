cb3a227e4340584c1c741ba0822da4d31a2aece6
Nikolov0080 <apofis12343@gmail.com>
2021-01-14 19:25:37 +0200

    created; sorting buttons on home page

--- src/components/home/landing/index.js
+++ src/components/home/landing/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { ButtonGroup, Button, Container } from 'react-bootstrap';
 import getAllRecipes from '../../../controllers/recipes/GET/getAllRecipes/index';
 import Loading from '../../loading/index';
+import UserRecipes from '../../recipes/recipesList';
 import sortingAlgorithms from './sortingAlgorithms';
 
 const {
@@ -13,21 +14,42 @@ const {
 const Landing = () => {
 
     const [filtered, setFiltered] = useState([]);
+    const [recipes, setRecipes] = useState([])
     const [loading, setLoading] = useState(true);
     const [empty, setEmpty] = useState(false);
 
-
-
-    useEffect(() => {
+    const getRecipes = () => {
         getAllRecipes().then(({ data }) => {
             if (!Array.isArray(data)) {
                 setEmpty(true);
             }
-
-            setFiltered(data.slice(0, 6))
+            setRecipes(data);
             setLoading(false);
         })
-    }, [])
+    }
+
+    useEffect(() => {
+        getRecipes();
+
+    }, []);
+
+    const handleClick = (buttonIndex) => {
+        switch (buttonIndex) {
+            case 0:
+
+                setFiltered(byLikes(recipes))
+                break;
+            case 1:
+
+                setFiltered(byDifficulty(recipes))
+                break;
+            case 2:
+
+                setFiltered(mostEasy(recipes))
+                break;
+        }
+    }
+
 
     console.log(filtered)
     if (empty) {
@@ -45,14 +67,19 @@ const Landing = () => {
     return (
         <div>
             <Container className='mt-5'>
-
                 <ButtonGroup className="d-flex justify-content-center">
-                    <Button className="mr-2 ml-2" variant="outline-primary">Most Liked</Button>
-                    <Button className="mr-2 ml-2" variant="outline-primary">Most Difficult</Button>
-                    <Button className="mr-2 ml-2" variant="outline-primary">Easiest recipes</Button>
+                    <Button onClick={() => handleClick(0)} className="mr-2 ml-2" variant="outline-primary">Most Liked</Button>
+                    <Button onClick={() => handleClick(1)} className="mr-2 ml-2" variant="outline-primary">Most Difficult</Button>
+                    <Button onClick={() => handleClick(2)} className="mr-2 ml-2" variant="outline-primary">Easiest recipes</Button>
                 </ButtonGroup>
             </Container>
 
+            {filtered.length !== 0
+                ?
+                <UserRecipes recipes={filtered.slice(0, 6)} />
+                :
+                <UserRecipes recipes={recipes.slice(0, 6)} />
+            }
         </div>
     )
 }
--- src/components/recipes/recipesList.js
+++ src/components/recipes/recipesList.js
@@ -9,18 +9,15 @@ const UserRecipes = ({ func, recipes }) => {
             <hr />
             <h1 className="text-center">My recipes</h1>
             <hr />
-
             <Container>
                 <Row>
                     {recipes.map((recipe, index) => {
                         return <Col key={index}><Recipe func={func} data={recipe} /></Col>
                     })}
                 </Row>
-
             </Container>
-
         </div>
     )
 }
 
-export default UserRecipes
+export default UserRecipes;
